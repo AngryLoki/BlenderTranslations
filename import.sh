0c955afb9fceaf0bf3a980b444326b81cb1183e7
Sv. Lockal <lockalsash@gmail.com>
2017-07-30 22:08:25 +0300

    Sync with upstream

---
+++ import.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget https://svn.blender.org/svnroot/bf-translations/branches/ru/ru.po -O blender/ru.po
+wget https://svn.blender.org/svnroot/bf-translations/branches/blender.pot -O blender.pot
